1341b5ad5c67a94e0ee423b6227ed219922b6b5a
northfun <fanbeishuang@zhongan.io>
2018-09-05 15:24:19 +0800

    configurable hashlength

--- routing/config.go
+++ routing/config.go
@@ -4,7 +4,7 @@ import (
 	"time"
 )
 
-type Config struct {
+type tbConfig struct {
 	alpha              int
 	HashLength         int
 	hashBits           int
@@ -20,42 +20,27 @@ type Config struct {
 	revalidateInterval time.Duration
 }
 
-func (c *Config) SetDefault() {
+func (c *tbConfig) SetDefault() {
 	c.alpha = 3
-	c.HashLength = 40
-	c.hashBits = c.HashLength * 8
 	c.findsize = 16
 	c.bucketSize = 16
 	c.maxFindFailures = 5
-	c.nBuckets = c.hashBits / 15 //10 21
 	c.seedCount = 30
 	c.seedMaxAge = 7 * 24 * time.Hour
 	c.maxReplacements = 10
-	c.bucketMinDistance = c.hashBits - c.nBuckets //0~151,152,..,160
 	c.refreshInterval = 30 * time.Second
 	c.revalidateInterval = 30 * time.Second
+	c.updateHashLength(40)
 }
 
-var c *Config
-var dbc *DBConfig
-
-func init() {
-	initConfig()
+func (c *tbConfig) updateHashLength(v int) {
+	c.HashLength = v
+	c.hashBits = c.HashLength * 8
+	c.nBuckets = c.hashBits / 15                  //10 21
+	c.bucketMinDistance = c.hashBits - c.nBuckets //0~151,152,..,160
 }
 
-func initConfig() {
-	if c == nil {
-		c = &Config{}
-		c.SetDefault()
-	}
-
-	if dbc == nil {
-		dbc = &DBConfig{}
-		dbc.SetDefault()
-	}
-}
-
-type DBConfig struct {
+type dbConfig struct {
 	nodeDBNilHash        Hash          // Special node ID to use as a nil element.
 	nodeDBNodeExpiration time.Duration // Time after which an unseen node should be dropped.
 	nodeDBCleanupCycle   time.Duration // Time period for running the expiration task.
@@ -68,7 +53,7 @@ type DBConfig struct {
 	nodeDBDiscoverFindFails string
 }
 
-func (c *DBConfig) SetDefault() {
+func (c *dbConfig) SetDefault() {
 	c.nodeDBNilHash = NewHash()             // Special node ID to use as a nil element.
 	c.nodeDBNodeExpiration = 24 * time.Hour // Time after which an unseen node should be dropped.
 	c.nodeDBCleanupCycle = time.Hour        // Time period for running the expiration task.
@@ -77,4 +62,46 @@ func (c *DBConfig) SetDefault() {
 	c.nodeDBDiscoverPing = c.nodeDBDiscoverRoot + ":lastping"
 	c.nodeDBDiscoverPong = c.nodeDBDiscoverRoot + ":lastpong"
 	c.nodeDBDiscoverFindFails = c.nodeDBDiscoverRoot + ":findfail"
+}
+
+type Configurable struct {
+	HashLength int
+}
+
+func NewConfigurable() *Configurable {
+	return &Configurable{}
+}
+func NewCgWithParam(hl int) *Configurable {
+	return &Configurable{
+		HashLength: hl,
+	}
+}
+
+var c *tbConfig
+var dbc *dbConfig
+
+// Init method initializes params used by the module,
+// so call it before using any other methods in this package.
+// Or it will call panic
+func Init(cg *Configurable) {
+	initConfig(cg)
+}
+
+func initConfig(cg *Configurable) {
+	if cg == nil {
+		cg = NewConfigurable()
+	}
+	if c == nil {
+		c = &tbConfig{}
+		c.SetDefault()
+	}
+	if cg.HashLength > 0 {
+		c.updateHashLength(cg.HashLength)
+	}
+	if dbc == nil {
+		dbc = &dbConfig{}
+		dbc.SetDefault()
+	}
+	return
+
 }
